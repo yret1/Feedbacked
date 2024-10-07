d2669009b97c5c1dd2edd8fba724a025d42a8b98
Simon Gustavsson <simgus1234@gmail.com>
2024-10-07 13:46:48 +0200

    Push

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+  {
+    title: 'Home',
+    path: '',
+    component: HomeComponent,
+  },
+];
