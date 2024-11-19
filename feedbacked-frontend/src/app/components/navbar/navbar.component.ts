35219acf9387790b08813a38073fbd380c6cc228
Simon Gustavsson <simgus1234@gmail.com>
2024-11-19 15:54:18 +0100

    Mobile and screen nav for non authroutes done

--- feedbacked-frontend/src/app/components/navbar/navbar.component.ts
+++ feedbacked-frontend/src/app/components/navbar/navbar.component.ts
@@ -4,6 +4,14 @@ import { filter } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../../services/auth';
 import { CommonModule } from '@angular/common';
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
@@ -11,19 +19,69 @@ import { CommonModule } from '@angular/common';
   imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('openClose', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.3s ease-in-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-in-out', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('linkStagger', [
+      transition(':enter', [
+        query(
+          'li',
+          [
+            style({ opacity: 0, transform: 'translateY(20px)' }),
+            stagger(100, [
+              animate(
+                '300ms ease-out',
+                style({
+                  opacity: 1,
+                  transform: 'translateY(0)',
+                })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   constructor(private router: Router, private authService: AuthService) {}
 
   navVersion: 'Landing' | 'Dash' | 'Client' | 'Auth' = 'Landing';
 
+  links = [
+    { name: 'Home', route: '/' },
+    { name: 'Integrations', route: '/integrations' },
+    { name: 'Pricing', route: '/pricing' },
+    { name: 'Why', route: '/why' },
+    { name: 'Contact', route: '/contact' },
+  ];
+
+  dashlinks = [
+    { name: 'Dashboard', route: '/dashboard' },
+    { name: 'Settings', route: '/dashboard/settings' },
+  ];
+
   routeTo = '/';
   private routerSubscription: Subscription | undefined;
 
   scrolled = false;
+  open = false;
 
   signout() {
     this.authService.logout();
+    this.open = false;
+  }
+
+  openMenu() {
+    this.open = !this.open;
   }
 
   ngOnInit() {
