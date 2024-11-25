96f6fe1db94f7ef271db6c915ae6b004062254df
Simon Gustavsson <simgus1234@gmail.com>
2024-11-25 09:42:46 +0100

    Redesign pending

--- feedbacked-backend/schemas/clientmodel.ts
+++ feedbacked-backend/schemas/clientmodel.ts
@@ -3,8 +3,9 @@ const mongo = require("mongoose");
 const clientModel = new mongo.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String, required: false },
   status: { type: String, required: true, default: "active" },
+  url: {type: String, required: true},
   issues: [],
   created_at: { type: Date, default: Date.now },
 });
